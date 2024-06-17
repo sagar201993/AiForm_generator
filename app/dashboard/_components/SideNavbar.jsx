defa5cba23cde5b415ebfd046bdbf4140eeea61d
sagar <sagar201993>
2024-06-17 14:42:33 +0200

    created usepath for every page

--- app/dashboard/_components/SideNavbar.jsx
+++ app/dashboard/_components/SideNavbar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { LibraryBig, LineChart, MessageSquare, Shield } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const SideNavbar = () => {
   const menuList = [
@@ -7,34 +9,37 @@ const SideNavbar = () => {
       id: 1,
       name: "My Forms",
       icon: LibraryBig,
-      path: "/",
+      path: "/dashboard",
     },
     {
       id: 2,
       name: "Responses",
       icon: MessageSquare,
-      path: "/",
+      path: "/dasboard/responses",
     },
     {
       id: 1,
       name: "Analytics",
       icon: LineChart,
-      path: "/",
+      path: "/dasboard/analytics",
     },
     {
       id: 1,
       name: "Upgrade",
       icon: Shield,
-      path: "/",
+      path: "/dasboard/upgrade",
     },
   ];
+  const path = usePathname();
+  useEffect(() => {}, [path]);
   return (
     <div className="h-screen shadow-md">
       <div className="p-5">
         {menuList.map((menu, index) => (
           <h2
             key={index}
-            className="flex items-end cursor-pointer gap-3 p-4 hover:bg-primary hover:text-white rounded-lg"
+            className={`flex items-end cursor-pointer gap-3 p-4 hover:bg-primary hover:text-white rounded-lg mb-3
+             text-gray-500 ${path == menu.path && "bg-primary text-white"}`}
           >
             <menu.icon />
             {menu.name}
